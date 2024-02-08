0634bc4abad51642ba4f0ebf91ae224a842d21d3
Bernard BDLT <bernarddelatortue@gmail.com>
2024-02-08 11:45:22 +0100

    fin des beugs

--- trucsocket.py
+++ trucsocket.py
@@ -1,12 +1,8 @@
 import socket
 import sys
 
-# Définition des informations du serveur
-server_ip = '172.18.193.60'  # Adresse IP du serveur
-server_port = 2000  # Port du serveur
-timeout = 10  # Délai maximum d'attente pour la réponse du serveur (en secondes)
-
-# Création d'un objet socket
+server_ip = '172.18.193.60'  
+server_port = 2000  
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connexion au serveur
@@ -16,10 +12,6 @@ except ConnectionRefusedError:
     print("Impossible de se connecter au serveur.")
     sys.exit(1)
 
-# Configuration du délai d'attente pour la réception des données
-client_socket.settimeout(timeout)
-
-# Boucle de communication avec le serveur
 while True:
     # Réception des données du serveur
     try:
@@ -39,18 +31,6 @@ while True:
 
     # Envoi de la réponse au serveur
     client_socket.send(response.encode())
-
-    # Réception de la réponse du serveur
-    try:
-        data = client_socket.recv(1024).decode()
-    except socket.timeout:
-        print("La réponse du serveur a pris trop de temps. Fermeture de la communication.")
-        break
-
+    data = client_socket.recv(1024).decode()
     print("Réponse du serveur:", data)
-
-    # Vérification si la communication doit être terminée
-    
-
-# Fermeture de la connexion
 client_socket.close()
