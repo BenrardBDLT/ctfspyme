0d5e27b4d49740dd005a97330703a593353b91a9
Bernard BDLT <bernarddelatortue@gmail.com>
2024-02-13 16:33:37 +0100

    aze

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('truc.j2')
-
+@app.route('/epreuve1')
+def page1():
+    return render_template('epreuve1.j2')
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True)
  
