bd701fb3489a7cde64784dfb21842fda44a13f59
Bernard BDLT <bernarddelatortue@gmail.com>
2024-02-15 11:59:43 +0100

    a

---
+++ socket/reposesovketrobinou.py
@@ -0,0 +1,27 @@
+import socket
+import sys
+
+server_ip = '172.18.194.208'
+server_port = 2000
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connexion au serveur
+try:
+    client_socket.connect((server_ip, server_port))
+except ConnectionRefusedError:
+    print("Impossible de se connecter au serveur.")
+    sys.exit(1)
+
+while True:
+    # Réception des données du serveur
+    data = client_socket.recv(1024).decode()
+    print(data)
+    result = eval(data)
+   
+
+    # Envoi de la réponse au serveur
+    client_socket.send(str(result).encode())
+    data = client_socket.recv(1024).decode()
+    print("Réponse du serveur:", data)
+
+client_socket.close()
