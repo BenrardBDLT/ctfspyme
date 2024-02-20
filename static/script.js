b199638310c1db9dc6bc158d88909177e19252f8
bernard_BDLT <bernatonkawaii@gmail.com>
2024-02-20 22:49:51 +0100

    esth"tique du site + figma

---
+++ static/script.js
@@ -0,0 +1,9 @@
+const menuHamburger = document.querySelector(".menu-hamburger");
+    const navLinks = document.querySelector(".nav-links");
+
+    menuHamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+        menuHamburger.classList.toggle('active');
+    });
+    
+    
