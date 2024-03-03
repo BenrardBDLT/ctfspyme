ae16fc40d54857788286a9438fd304b4740119d1
bernard_BDLT <bernatonkawaii@gmail.com>
2024-03-03 21:18:29 +0100

    ça m'enerve :D

--- static/script.js
+++ static/script.js
@@ -9,4 +9,16 @@ const menuHamburger = document.querySelector(".menu-hamburger");
 
     
     });
-    
+    document.addEventListener('DOMContentLoaded', function() {
+        const epreuves = document.querySelectorAll('.container');
+
+        epreuves.forEach(epreuve => {
+            epreuve.addEventListener('click', function() {
+                // Trouve le contenu de l'épreuve dans ce container
+                const epreuveContent = this.querySelector('.epreuve-content');
+
+                // Toggle la classe 'open' pour afficher ou cacher le contenu
+                epreuveContent.classList.toggle('open');
+            });
+        });
+    });
