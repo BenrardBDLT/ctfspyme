7e7e48fec8d8e6ee0c6dd050028422ae29a7f2f6
bernard_BDLT <bernatonkawaii@gmail.com>
2024-03-03 21:00:42 +0100

    tout ce que hj'adore

--- static/script.js
+++ static/script.js
@@ -1,9 +1,12 @@
 const menuHamburger = document.querySelector(".menu-hamburger");
     const navLinks = document.querySelector(".nav-links");
 
+
     menuHamburger.addEventListener('click', () => {
         navLinks.classList.toggle('active');
         menuHamburger.classList.toggle('active');
-    });
+
+
     
+    });
     
